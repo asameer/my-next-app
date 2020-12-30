d74aaba63c978864b00cfde73169365501650e47
asamir <a.shalapy@gmail.com>
2020-12-30 22:01:12 +0200

    deploy with git initegration to vercel

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js! on vercel</a>
+        Deploy with git integration to vercel 
         </h1>
 
         <p className={styles.description}>
